0b30bcf0814f4475512333afcfd5365b26a5136f
JosueAFerrin <93354595+JosueAFerrin@users.noreply.github.com>
2025-08-07 08:39:07 -0500

    Arreglo de archivo eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
 export default [
   {
     files: ['**/*.js'],
-    languuageOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-  },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
     rules: {
-        semi: ['error', 'always'],
-        quotes: ['error', 'single']
+      semi: ['error', 'always'],
+      quotes: ['error', 'single']
     }
   }
 ];
